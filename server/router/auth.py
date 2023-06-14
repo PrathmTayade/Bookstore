74d697da12358ab0e7a8aca5f08c163ee2d06b8f
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-06-14 11:48:15 +0530

    so many fked up changes

--- server/router/auth.py
+++ server/router/auth.py
@@ -1,14 +1,10 @@
-from fastapi import APIRouter
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pymongo import MongoClient
-from passlib.context import CryptContext
-from database.db import users_collection
+from fastapi import Depends, HTTPException, APIRouter
 from pydantic import BaseModel
-
-
-auth_router = APIRouter()
-security = HTTPBasic()
-pwd_context = CryptContext(schemes=["bcrypt"])
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from database.db import users_collection
+from jose import JWTError, jwt
 
 
 class User(BaseModel):
@@ -17,20 +13,68 @@ class User(BaseModel):
     password: str
 
 
-@auth_router.post("/login")
-async def login(credentials: HTTPBasicCredentials):
-    print(credentials)
-    password = pwd_context.verify(
-        credentials.password,
-        "$2b$12$sIjA79Uk2Ky6xrmAsBw64u42q4vnRxKPjYjVMgGGmE9/lIaiJuVnG",
-    )
+SECRET_KEY = "analystai"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-    return {"message": "Login successful", "password": password}
+auth_router = APIRouter()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(username: str):
+    user = users_collection.find_one({"username": username})
+    if user:
+        return user
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user["password"]):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@auth_router.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    print("asdsad")
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user["username"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @auth_router.post("/signup")
-def signup(user: User):
-    hashed_password = pwd_context.hash(user.password)
-    user_data = {"email": user.email, "password": hashed_password}
-
-    return user_data
+async def signup(user: User):
+    hashed_password = get_password_hash(user.password)
+    new_user = {"name": user.name, "email": user.email, "password": hashed_password}
+    users_collection.insert_one(new_user)
+    return {"message": "User created successfully"}
